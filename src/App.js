72cca0e26ab174249e91f3c2320e09e286d3a0ea
levaniakobidze <levaniakobidze25@gmail.com>
2022-03-27 17:19:54 +0400

    LoginPage completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import LoginPage from './pages/LoginPage/LoginPage';
+import WelcomePage from './pages/WelcomePage/WelcomePage';
 
 function App() {
   return (
     <div className="App">
-     <LoginPage />
+    <WelcomePage />
     
     </div>
   );
