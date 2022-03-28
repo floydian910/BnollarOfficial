9270f428aea4f48977a0623711ac2982e7a3d9f4
unknown <skubidu2@inbox.ru>
2022-03-28 18:08:39 +0400

    swiper-done

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Feed from '../../components/FeedComponents/Feed/Feed'
+import "./HomePage.css"
+const HomePage = () => {
+  return (
+    <div className="home_page_wrapper">
+        <Feed/>
+    </div>
+  )
+}
+
+export default HomePage
