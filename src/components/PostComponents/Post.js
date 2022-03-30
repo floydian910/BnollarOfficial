6047c324e5077f7cab0a8ea4f11c1a3bc6ae3ac5
levaniakobidze <levaniakobidze25@gmail.com>
2022-03-30 21:00:17 +0400

    Home/page Left/bar and center fully completed

--- src/components/PostComponents/Post.js
+++ src/components/PostComponents/Post.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import './Post.css'
 import {PostData} from './PostData'
+import heart from '../icons/heart.svg'
+import  Comment from '../icons/Comment.svg'
+import Send from '../icons/Send.svg'
+import Stroke from '../icons/Stroke.svg'
+
 
 
 function Post() {
@@ -10,25 +15,64 @@ function Post() {
 
                     return (<>
                             <div className="post-content">
+
+                      {/********************** HEADER************************************/}          
                             <div className="post-header">
                             <div className="post-profile-image">  
                              <img src={post.profileImg} alt="image" />
                             </div>
+                      {/**********************************************************/}
+
+
+
+
+
+                      {/********************* NICKNAME AND NAME*************************************/}      
                             <div className="name-and-nick">
-                         
                                 <h3>{post.name}</h3>
-                            
-                             
-                                    <p>{post.nickName}</p>
-                        
+                                    <p>{post.nickName}</p>                        
                                 </div>
                             </div>
-                        
+                      {/**********************************************************/}  
+
+
+
+                     {/************************ LOGO AND SEARCH **********************************/}
                             <div className="post-main-img">
                             <img src={post.img} alt="img" />
                         </div>
-                        </div>
+                        
+                     {/**********************************************************/}
+                        
+
+                     
+                    {/********************* POST-FUNCTIONS*************************************/}
+
+                                <div className="post-functions">
+
+                                        <div className="post-icons">
+                                                <div className="action-icons">
+                                                    <img src={heart} alt="heart" />
+                                                    <img className='comment-icon' src={Comment} alt="comment" />
+                                                    <img src={Send} alt="heart" />
+                                                </div>
+
+                                                <img className='stroke' src={Stroke} alt="stroke" />
+                                        </div>
+
+
+                                        <div className="likes-counter"> 5 likes</div>
+
+                                </div>
                       
+
+                      <div className="comment-wrapper">
+                          <input type="text"  placeholder='Write your comment' />
+                      </div>
+
+
+
+                     </div>
                         </>
 
                     )
