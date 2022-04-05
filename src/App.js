1f3a866058a1e1bb6c393c6963dc49af3416f65f
levaniakobidze <levaniakobidze25@gmail.com>
2022-04-05 14:20:30 +0400

    home last commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import HomePage from "./pages/HomePage/HomePage";
+import Settings from "./pages/Settings/Settings";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/profile" element={<HomePage />} />
+          <Route path="/settings" element={<Settings />} />
         </Routes>
       </BrowserRouter>
 
---
+++ src/pages/Settings/Settings.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Settings.css'
+import Container from '../../components/HomeComponents/HomeContainer/Container'
+function Settings() {
+  return (
+    <Container>
+
+            
+
+
+    </Container>
+  )
+}
+
+export default Settings
