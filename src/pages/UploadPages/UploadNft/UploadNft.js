7e146a4a5a64eebe89a1c31b0f533a6dc91a2143
levan-iakobidze <levaniakobidze25@gmail.com>
2022-07-12 21:02:27 +0400

    added place bid page

--- src/pages/UploadPages/UploadNft/UploadNft.js
+++ src/pages/UploadPages/UploadNft/UploadNft.js
@@ -61,7 +61,6 @@ function UploadNft() {
           </Link>
           <Link to={"/placebid"} className='upload-nft-upload-btn'>
             Upload
-          </Link>
         </div>
       </div>
     </Container>
